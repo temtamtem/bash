d6f76ef2395fb42c4aa433be7afd26fdb4748fa7
temtamtem <marek.huan77@gmail.com>
2024-06-18 14:55:50 +0200

    SFD

---
+++ hello.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Method 1
+sayHello1() {
+   echo "Hello"
+}
+
+# Method 2
+function sayHello2 {
+   echo "Hello Again"
+}
---
+++ zarcie.sh
@@ -0,0 +1,8 @@
+fryty() {
+read -p "masz ochote na fryty? " copedziol
+echo $copedziol
+}
+kebab() {
+read -p "masz ochote na kebsa? " copedziol
+echo $copedziol
+}
